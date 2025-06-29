469fa4e55e5a2217f6cf67c0da92da097d65ae8d
goutham4331 <goutham4331@gmail.com>
2025-06-29 14:01:41 -0500

    updating links to backend

--- frontend/src/links.ts
+++ frontend/src/links.ts
@@ -1,3 +1,7 @@
-export const backendUrl = "http://localhost:5000/api/generate-description";
-export const backendUrlconvert = "http://localhost:5000/api/convert";
-export const backendUrlGenKey = "http://localhost:5000/api/keywords-generator";
+// export const backendUrl = "http://localhost:5000/api/generate-description";
+// export const backendUrlconvert = "http://localhost:5000/api/convert";
+// export const backendUrlGenKey = "http://localhost:5000/api/keywords-generator";
+
+export const backendUrl = "https://ai-resume-editor-backend.onrender.com/api/generate-description";
+export const backendUrlconvert = "https://ai-resume-editor-backend.onrender.com/api/convert";
+export const backendUrlGenKey = "https://ai-resume-editor-backend.onrender.com/api/keywords-generator";
